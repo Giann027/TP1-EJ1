bb3fa8a7c9843dd9b19b27e979ddf6054201483d
ricot <ricot@DESKTOP-FTHKA14>
2021-06-07 15:58:37 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TP1_EJ1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Suma de dos Números ";
+            int Numero1;
+            int Numero2;
+            int Suma;
+            Console.Write("Ingrese El Primer Número:");
+            Numero1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Ingrese El Segundo Número:");
+            Numero2 = Convert.ToInt32(Console.ReadLine());
+
+            Suma = (Numero1 + Numero2);
+            Console.Write($"El resultado de la suma es {Suma}");
+        }
+    }
+}
